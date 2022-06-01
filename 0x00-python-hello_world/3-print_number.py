3f0ae61652a327837862187a636b97ba27d31dd3
lyaef <lyaef58@gmail.com>
2022-06-01 19:33:06 +0300

    New project

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 98
+print('{number}, Battery street[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Df'[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C'{number} Battery street\n')
